be37fa701fcf041686f49defb39e4b8b3c648ce4
victor <vfinamore@gmail.com>
2019-03-19 16:55:35 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,27 +34,17 @@ public class Program {
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
-			
-			//Tratamento de forma ruim.
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates.");
+
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
 			}
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out dates for update must be future dates.");
-			} 
-			
-			else {
-			//Tratamento ruim termina aqui
-			reservation.updateDates(checkIn, checkOut);
-			System.out.println("Reservation: " + reservation);
-		}
-		
-		}		
-		
-		
+			else  {
+				System.out.println("Reservation: " + reservation);
+			}
 		
 		sc.close();
+		}
 	}
+}	
 
-}
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -40,9 +40,18 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	//Update dates - atualizar o checkin e chackout.
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for update must be future dates.";
+		}
+		if (!checkOut.after(checkIn)) {
+			return "Check-out dates for update must be future dates.";
+		} 
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
 	
 	//Implementando o toString, imprimindo a reserva conforme texto(colocar o @Override)
